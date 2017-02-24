f3abc3ef5b02ec70026b1efca0987c41acdde21c
Ivan Mattie <mattieivan@gmail.com>
2017-02-23 17:57:46 -0700

    Added all tests for set()

--- test/StorageManagerTests.js
+++ test/StorageManagerTests.js
@@ -10,5 +10,17 @@ describe("StorageManager", function() {
 
             assert.equal(manager.data["foo"], "bar", "Data's foo entry should be 'bar'.");
         });
+
+        it("should set the correct value in the JSON file", function() {
+            manager.set("bar", "baz");
+            const fileData = require("./../storage/test.json");
+        
+            assert.equal(fileData.bar, "baz", "File did not contain the 'bar' property!");
+        });
+        it("should be retrievable via get", function() {
+            manager.set("baz", "foobar");
+
+            assert.equal(manager.get("baz"), "foobar", "get('baz') did not return 'foobar'.");
+        });
     });
 });
